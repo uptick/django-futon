799241a4f264bf5de0a0467b7a702200665b237e
Luke Hodkinson <furious-luke@gmail.com>
2015-11-23 09:44:57 +1100

    Found some bad references.

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-    name='django-address',
-    version='0.2',
+    name='django-futon',
+    version='0.1',
     author='Luke Hodkinson',
     author_email='furious.luke@gmail.com',
     maintainer='Luke Hodkinson',
     maintainer_email='furious.luke@gmail.com',
-    url='https://github.com/furious-luke/django-address',
-    description='A django application for describing addresses.',
+    url='https://github.com/furious-luke/django-futon',
+    description='A lightweight application for synchronising Django models between sites using RESTful APIs.',
     long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     classifiers = [
         'Development Status :: 3 - Alpha',
@@ -21,14 +21,13 @@ setup(
         'Programming Language :: Python',
     ],
     license='BSD',
-
     packages=find_packages(),
     include_package_data=True,
     package_data={'': ['*.txt', '*.js', '*.html', '*.*']},
     install_requires=[
         'setuptools',
-        'geopy',
         'six',
+        'requests',
     ],
     zip_safe=False,
 
