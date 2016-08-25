927d19b5e6c8ac554ded0b36a513e9f174786920
Luke Hodkinson <furious.luke@gmail.com>
2016-08-25 15:40:45 +1000

    Check settings for global credentials.

--- futon/credentials.py
+++ futon/credentials.py
@@ -7,8 +7,12 @@ __all__ = ['get_credentials']
 
 def _get(name, ext):
     return settings.FUTON_SITES.get(name.lower(), {}).get(
-        ext, os.environ.get(name.upper() + '_' + ext.upper(),
-                            os.environ.get('FUTON_DEFAULT_' + ext.upper(), None)))
+        ext,
+        getattr(
+            settings, 'FUTON_DEFAULT_' + ext.upper(),
+            os.environ.get(
+                name.upper() + '_' + ext.upper(),
+                os.environ.get('FUTON_DEFAULT_' + ext.upper(), None))))
 
 
 def get_credentials(site):
