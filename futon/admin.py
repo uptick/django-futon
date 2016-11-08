0a3ef35bed158c41ab08a9fbf7cb80a0010c59b8
Luke Hodkinson <furious.luke@gmail.com>
2016-11-08 12:38:19 +1100

    Dammit. Forgot the admin.

--- futon/admin.py
+++ futon/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Token
+from .models import Token, Site
+
+
+@admin.register(Site)
+class SiteAdmin(admin.ModelAdmin):
+    pass
 
 
 @admin.register(Token)
